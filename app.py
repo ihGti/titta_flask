3dec3a0690f00fda796fe7f33ca9ab60731bc68b
yamatatsu1215 <tatsuyama3140@gmail.com>
2023-11-27 15:02:09 +0900

    '課題用のファイル'

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 from project import create_app
-from project.config import SQLALCHEMY_DATABASE_URI
-
-SQLALCHEMY_DATABASE_URI = 'mysql://titta_project:2023$Iht!tta@localhost/titta_db'
 
 app = create_app()
 
