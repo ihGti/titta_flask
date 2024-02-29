2a1db7df38f440f7439f63cd392c076089859848
Weru7e <r-tomipc@outlook.jp>
2024-02-29 13:13:42 +0900

    totyuu

--- project/static/js/index.js
+++ project/static/js/index.js
@@ -1,14 +1,25 @@
 
-
-
-$(function () {
-    $('#slider_js').slick({
-      arrows: true, // 前・次のボタンを表示する
-      dots: true, // ドットナビゲーションを表示する
-      appendDots: $('.S_dots'), // ドットナビゲーションの生成位置を変更
-      speed: 1000, // スライドさせるスピード（ミリ秒）
-      slidesToShow: 1, // 表示させるスライド数
-      centerMode: true, // slidesToShowが奇数のとき、現在のスライドを中央に表示する
-      variableWidth: true, // スライド幅の自動計算を無効化
-    });
+$('.slider').slick({
+    autoplay: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: '<div class="slick-prev"></div>',
+    nextArrow: '<div class="slick-next"></div>',
+    centerMode: true,
+    variableWidth: true,
+		dots: true,
   });
+
+    $(".slider_sJs").slick({
+      arrows: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      prevArrow: '<img src="/static/images/slick_prevI.png" class="slide-arrow prev-arrow">',
+      nextArrow: '<img src="/static/images/slick_nextI.png" class="slide-arrow next-arrow">',
+      variableWidth: true,
+		  dots: true,
+    });
